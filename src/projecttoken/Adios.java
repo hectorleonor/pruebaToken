34e4637ad557f442488fec703f77a51fe37d14ec
hectorleonor <hector.leonor@educa.madrid.org>
2025-02-11 12:36:07 +0100

    v2 nueva Clase

---
+++ src/projecttoken/Adios.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projecttoken;
+
+/**
+ *
+ * @author hector.leomol
+ */
+public class Adios {
+    public static void main(String[] args) {
+        System.out.println("Adios Mundo");
+    }
+}
